02fcd2eaa82e56352447aa9b14dbab46c6e630f8
GlacialDreamss <82543629+GlacialDreamss@users.noreply.github.com>
2022-05-12 14:49:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pygame as pg
 
-class Screen: 
+class Screen: #Calls the refresh method using parameters to determine its size
     while True:
         print(WIP)
 
-def refresh():
+def refresh(width, height):
     fps = 60
     while True:
         for num in range(0,fps):
