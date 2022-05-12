aef787f8e93ae59db6f0902f8fce05b667a14a5e
GlacialDreamss <82543629+GlacialDreamss@users.noreply.github.com>
2022-05-12 14:39:23 +0100

    Waffle

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import pygame as pg
 
 class Screen: 
     while True:
-        Screen.blip
+        print(WIP)
+
+def refresh():
+    fps = 60
+    while True:
+        for num in range(0,fps):
+            Screen.fill((0,0,0))
+
