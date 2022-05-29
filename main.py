56610756856a91a714fcf3cf5342eae41626e9db
GlacialDreamss <danieladetokunbo05@gmail.com>
2022-05-29 21:05:22 +0100

    Yee

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-import pygame as pg
+import pygame
 
-class Screen: #Calls the refresh method using parameters to determine its size
-    width = 1920
-    height =1080
-    refresh(width, height)
-    print()
+#Window Variables
+background_Colour = (32,64,128)
+(width, height) = (300, 200)
 
-def refresh(width, height):
-    fps = 60
-    while True:
-        for num in range(0,fps):
-            Screen.fill((0,0,0))
+#Window properties being assigned
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Deez")
+screen.fill(background_Colour)
 
+#Window running, main loop
+pygame.display.flip() #Flip displays the display
+running = True #Flag for whether or not the program should close
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+  
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            screen.fill((255,255,255))
+            pygame.display.flip()
--- pytesseract.py
+++ pytesseract.py
@@ -1,2 +1,3 @@
+import pytesseract
 class tess:
     pytesseract.pytesseract.tesseract_cmd = 'C:\Users\danie\AppData\Local\Programs\Tesseract-OCR'
