d39a3ed7fdd6b481d9af9fcde0d4622459457c3d
GlacialDreamss <82543629+GlacialDreamss@users.noreply.github.com>
2022-06-20 08:04:20 +0100

    Thanks Filip
    :)

--- main.py
+++ main.py
@@ -20,37 +20,32 @@ class Game():
         e.mouse = pgm.mouse.get_pressed()
         e.mousePos = pgm.mouse.get_pos()
 
+        e.posList = []
+
     #Main function
     def main(e):
         while True:
             e.scourge()
-
-            e.prevKeys = e.keys
-            e.keys = pgm.key.get_pressed()
+            if e.mouse[0] and e.mousePos not in e.posList: e.posList.append(e.mousePos)
+            for pixel in e.posList: e.screen.set_at(pixel, (0, 0, 0))
 
     #Loop function
     def scourge(e):
-        while True:
-            pgm.display.set_caption("Deez")
-            e.clock.tick(e.fps)
-            pgm.display.update()
+        pgm.display.set_caption("Deez")
+        e.clock.tick(e.fps)
+        pgm.display.update()
+        e.screen.fill((255, 255, 255))
 
-            for event in pgm.event.get():
-                if event == pgm.QUIT:
-                    exit()
-                
-                #Somehow need to be able to see line progression and fill in the line. Can see line progression if the pixels are set outside the loop
-                if event.type == pgm.MOUSEBUTTONDOWN:
-                    print(e.mousePos)
-                    print(e.mouse)
-                    e.mousePosDown = pgm.mouse.get_pos()
-                    e.screen.set_at([e.mousePos[0], e.mousePos[1]], (0, 0, 0))
+        for event in pgm.event.get():
+            if event == pgm.QUIT:
+                exit()
 
-                if event.type == pgm.MOUSEBUTTONUP:
-                    e.mousePosUp = pgm.mouse.get_pos()
-                    while e.mousePosUp != e.mousePosDown:
-                        e.screen.set_at([e.mousePos[0], e.mousePos[1]], (0, 0, 0))
+        e.mouse = pgm.mouse.get_pressed()
+        e.mousePos = pgm.mouse.get_pos()
 
+        e.prevKeys = e.keys
+        e.keys = pgm.key.get_pressed()
+ 
 #Execution
 game = Game()
 game.main()
