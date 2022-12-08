c90ea35e0efbe8f1a5286f8b34356148ec16feb8
GlacialDreamss <82543629+GlacialDreamss@users.noreply.github.com>
2022-12-08 11:50:05 +0000

    nothing

--- pyqtwindow.py
+++ pyqtwindow.py
@@ -6,29 +6,21 @@ import sys
 
 from stylesheet import *
 
-
-def paint(e):
-        painter = QPainter(e)
-        pic = QPixmap("icon.png")
-        painter.drawPixmap(e.rect(), pic)
-        colour = QColor
-        # colour.setRed() #Where I stopped, assigning values for all 3 in rgb
-        # painter.setPen(colour)
-        # painter.drawRect(40, 40, 400, 200)
-
 class Window(QWidget):
     def __init__(e):
         QWidget.__init__(e)
-        layout = QGridLayout()
-        e.setLayout(layout)
+
+        e.layout = QGridLayout()
+        e.setLayout(e.layout)
         e.setWindowTitle("Typetone")
         screen_width, screen_height = 400, 400
         e.setGeometry(10, 150, screen_width, screen_height)
         e.setWindowIcon(QIcon("icon.png"))
 
+
         # create menu
         menubar = QMenuBar()
-        layout.addWidget(menubar, 0, 0)
+        e.layout.addWidget(menubar, 0, 0)
         for num in range(0, len(ui_sections.toolbar_section_list)):
             toolbar_section = menubar.addMenu(ui_sections.toolbar_section_list[num])
             
@@ -38,26 +30,36 @@ class Window(QWidget):
             toolbar_section.addSeparator
 
         # add labels 
-
+        text = QPlainTextEdit()
+        text2 = QPlainTextEdit()
         # add textboxes
         for num in range(2,5):
-            layout.addWidget(QPlainTextEdit(), num, 1)
+            e.layout.addWidget(text, num, 1)
             if num == 4: break
-            layout.addWidget(QPlainTextEdit(), num, 0)    
+            e.layout.addWidget(text2, num, 0)
+        
+        print(text.toPlainText())
         
         # add buttons for text to speech
         label = QLabel()
         label2 = QLabel()
         label.setText("Input")
         label2.setText("Output")        
-        layout.addWidget(label, 1, 0)
-        layout.addWidget(label2, 1, 1)        
+        e.layout.addWidget(label, 1, 0)
+        e.layout.addWidget(label2, 1, 1)        
         
         # Somehow do interface
-        paint(e)
+    
+    def paint(e):
+        painter = QPainter(e)
+        pic = QPixmap("icon.png")
+        painter.drawPixmap(e.rect(), pic)
+        colour = QColor
+        # colour.setRed() #Where I stopped, assigning values for all 3 in rgb
+        # painter.setPen(colour)
+        # painter.drawRect(40, 40, 400, 200)
 
 
-        
 app = QApplication(sys.argv)
 screen = Window()
 screen.show()
